6863c4721bf46118464449a082676a111a8e37c2
Polvere243 <danielercoli@gmail.com>
2023-12-18 21:10:33 +0100

    did the first two points

--- js/srcipt.js
+++ js/srcipt.js
@@ -1,1 +1,4 @@
-console.log("JS OK");
+// chiedere all'utente la distanza che vuole percorrere
+const distanceToGO = prompt("Inserisci la distanza che vuoi percorrere");
+// chiedere all'utente la sua età
+const passengerAge = prompt("Inserisci la tua età");
