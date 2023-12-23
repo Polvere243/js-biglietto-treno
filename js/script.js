0cd029b58aaac2392210f701291c7697a5050b27
Polvere243 <danielercoli@gmail.com>
2023-12-23 21:24:46 +0100

    added validation

--- js/script.js
+++ js/script.js
@@ -2,20 +2,23 @@
 const distanceToGO = prompt("Inserisci la distanza che vuoi percorrere");
 // chiedere all'utente la sua età
 const passengerAge = prompt("Inserisci la tua età", "30");
-// Calcolare il prezzo del biglietto moltiplicando la distanza per 0.21
+
+if (isNaN(distanceToGo) || isNaN(passegnerAge) || passengerAge < 0 || distanceToGo < 0){
+    alert("Devi inserire dei valori numerici")
+  
+} else {
+    // Calcolare il prezzo del biglietto moltiplicando la distanza per 0.21
 const minPrice = 0.21;
 console.log(minPrice);
 // Calcolare il prezzo in base alla distanza da percorrere
 let pricePerDistance = (distanceToGO * minPrice).toFixed(2);
 console.log(pricePerDistance);
 // Aggiungere lo sconto del 20% per i minori di vent'anni
-const youngAge = 18;
+const youngAge = 20;
 
-const oldAge = 65;
+const oldAge = 60;
 
 let discount; 
-
-
 
 if (passengerAge < youngAge) {
     discount = ((pricePerDistance / 100) * 20).toFixed(2);
@@ -26,3 +29,4 @@ if (passengerAge < youngAge) {
 }
 
 console.log(discount);
+}
