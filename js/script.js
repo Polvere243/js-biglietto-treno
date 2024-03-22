4b9199782ce7c72c0bd846c5946536421631f859
Polvere243 <danielercoli@gmail.com>
2024-03-22 20:31:01 +0100

    fixed some capital letters

--- js/script.js
+++ js/script.js
@@ -1,32 +1,32 @@
 // chiedere all'utente la distanza che vuole percorrere
-const distanceToGO = prompt("Inserisci la distanza che vuoi percorrere");
+const distanceToGo = parseInt(prompt("Inserisci la distanza che vuoi percorrere"));
 // chiedere all'utente la sua età
-const passengerAge = prompt("Inserisci la tua età", "30");
+const passengerAge = parseInt(prompt("Inserisci la tua età", "30"));
 
-if (isNaN(distanceToGo) || isNaN(passegnerAge) || passengerAge < 0 || distanceToGo < 0){
+if (isNaN(distanceToGo) || isNaN(passengerAge) || passengerAge < 0 || distanceToGo < 0) {
     alert("Devi inserire dei valori numerici")
-  
+
 } else {
     // Calcolare il prezzo del biglietto moltiplicando la distanza per 0.21
-const minPrice = 0.21;
-console.log(minPrice);
-// Calcolare il prezzo in base alla distanza da percorrere
-let pricePerDistance = (distanceToGO * minPrice).toFixed(2);
-console.log(pricePerDistance);
-// Aggiungere lo sconto del 20% per i minori di vent'anni
-const youngAge = 20;
+    const minPrice = 0.21;
+    console.log(minPrice);
+    // Calcolare il prezzo in base alla distanza da percorrere
+    let pricePerDistance = (distanceToGo * minPrice).toFixed(2);
+    console.log(pricePerDistance);
+    // Aggiungere lo sconto del 20% per i minori di vent'anni
+    const youngAge = 20;
 
-const oldAge = 60;
+    const oldAge = 60;
 
-let discount; 
+    let discount;
 
-if (passengerAge < youngAge) {
-    discount = ((pricePerDistance / 100) * 20).toFixed(2);
-} else if (passengerAge > oldAge){
-    discount = ((pricePerDistance /100)* 40).toFixed(2);
-} else if (passengerAge > youngAge && passengerAge < oldAge){
-    discount = 0
-}
+    if (passengerAge < youngAge) {
+        discount = ((pricePerDistance / 100) * 20).toFixed(2);
+    } else if (passengerAge > oldAge) {
+        discount = ((pricePerDistance / 100) * 40).toFixed(2);
+    } else if (passengerAge > youngAge && passengerAge < oldAge) {
+        discount = 0
+    }
 
-console.log(discount);
+    console.log(discount);
 }
